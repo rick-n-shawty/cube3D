59d46ef5d935c8502c3bc932e578641d360a048e
rick-n-shawty <sultanhoodie@gmail.com>
2024-07-07 16:22:40 +0800

    draw

--- include/Cube.hpp
+++ include/Cube.hpp
@@ -6,8 +6,15 @@ class Cube{
     private: 
         static const int verticesNum = 8;
         sf::Vector3f vectors[verticesNum];
+        sf::CircleShape shapes[verticesNum]; 
     public: 
         Cube(float length); 
-        ~Cube(); 
+        ~Cube();
+
+        void show(sf::RenderWindow& window){
+            for(int i = 0; i < verticesNum; i++){
+                window.draw(shapes[i]);
+            }
+        } 
 };
 #endif
