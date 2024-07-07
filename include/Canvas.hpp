dc04162fa6643bed10db89a34a2e87718e4e0975
rick-n-shawty <sultanhoodie@gmail.com>
2024-07-07 15:20:00 +0800

    canvas.hpp

--- include/Canvas.hpp
+++ include/Canvas.hpp
@@ -1,1 +1,16 @@
 #include <SFML/Graphics.hpp>
+
+
+class Canvas{
+    private: 
+        sf::RenderWindow window;
+
+        void handleEvents(); 
+        void update(); 
+        void render(); 
+        
+    public: 
+        Canvas();
+        ~Canvas(); 
+        void run(); 
+};
