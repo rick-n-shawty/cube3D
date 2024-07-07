62601cfff3edf2368c4f24a8fd832c875392dce5
rick-n-shawty <sultanhoodie@gmail.com>
2024-07-07 17:14:38 +0800

    scaling matrix

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -1,6 +1,7 @@
 #include "Canvas.hpp"
 
 float angle = 0; 
+float scale = 1; 
 float rotationMatrixX[3][3] = {
     {1, 0, 0},
     {0, cos(angle), -sin(angle)},
@@ -12,22 +13,34 @@ float rotationMatrixY[3][3] = {
     {-sin(angle), 0, cos(angle)}
 };
 float rotationMatrixZ[3][3] = {
-
+    {cos(angle), -sin(angle),0},
+    {sin(angle), cos(angle), 0},
+    {0,0,1}
+};
+float scalingMatrix[3][3] = {
+    {scale, 0, 0}, 
+    {0, scale, 0}, 
+    {0, 0, scale}
 };
 
 void updateMatrices(){
-    rotationMatrixX[0][0] = 1;              rotationMatrixY[0][0] = cos(angle); 
-    rotationMatrixX[0][1] = 0;              rotationMatrixY[0][1] = 0; 
-    rotationMatrixX[0][2] = 0;              rotationMatrixY[0][2] = sin(angle); 
+    rotationMatrixX[0][0] = 1;              rotationMatrixY[0][0] = cos(angle);     rotationMatrixZ[0][0] = cos(angle);  
+    rotationMatrixX[0][1] = 0;              rotationMatrixY[0][1] = 0;              rotationMatrixZ[0][1] = -sin(angle); 
+    rotationMatrixX[0][2] = 0;              rotationMatrixY[0][2] = sin(angle);     rotationMatrixZ[0][2] = 0; 
 
-    rotationMatrixX[1][0] = 0;              rotationMatrixY[1][0] = 0; 
-    rotationMatrixX[1][1] = cos(angle);     rotationMatrixY[1][1] = 1; 
-    rotationMatrixX[1][2] = -sin(angle);    rotationMatrixY[1][2] = 0;
+    rotationMatrixX[1][0] = 0;              rotationMatrixY[1][0] = 0;              rotationMatrixZ[1][0] = sin(angle);  
+    rotationMatrixX[1][1] = cos(angle);     rotationMatrixY[1][1] = 1;              rotationMatrixZ[1][1] = cos(angle);    
+    rotationMatrixX[1][2] = -sin(angle);    rotationMatrixY[1][2] = 0;              rotationMatrixZ[1][2] = 0;
 
-    rotationMatrixX[2][0] = 0;              rotationMatrixY[2][0] = -sin(angle);              
-    rotationMatrixX[2][1] = sin(angle);     rotationMatrixY[2][1] = 0;
-    rotationMatrixX[2][2] = cos(angle);     rotationMatrixY[2][2] = cos(angle);
+    rotationMatrixX[2][0] = 0;              rotationMatrixY[2][0] = -sin(angle);    rotationMatrixZ[2][0] = 0;              
+    rotationMatrixX[2][1] = sin(angle);     rotationMatrixY[2][1] = 0;              rotationMatrixZ[2][1] = 0; 
+    rotationMatrixX[2][2] = cos(angle);     rotationMatrixY[2][2] = cos(angle);     rotationMatrixZ[2][2] = 1;
 
+
+
+    scalingMatrix[0][0] = scale;
+    scalingMatrix[1][1] = scale;
+    scalingMatrix[2][2] = scale;
 }
 
 Canvas::Canvas(int width, int height) : cube(1){
@@ -60,10 +73,13 @@ void Canvas::handleEvents(){
     }
 }
 void Canvas::update(){
-    angle += 0.001;
+    angle += 0.0001;
+    scale += cos(angle); 
     updateMatrices();
     cube.multiplyVectors(rotationMatrixX);
     cube.multiplyVectors(rotationMatrixY);
+    cube.multiplyVectors(scalingMatrix);
+    // cube.multiplyVectors(rotationMatrixZ);
 }
 void Canvas::render(){
     window.clear(); 
